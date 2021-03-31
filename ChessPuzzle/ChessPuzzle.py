f663c4156adcaad1613654ced6d2087286f82c80
almoraboy <68150888+almoraboy@users.noreply.github.com>
2021-03-31 22:10:26 +0530

    ChessPath puzzle solution

--- ChessPuzzle/ChessPuzzle.py
+++ ChessPuzzle/ChessPuzzle.py
@@ -18,8 +18,8 @@ class ChessPiece:
 	def __repr__( self ):
 		return ChessPiece.pieceToTokenMap[ self.pieceName ]
 
-	def positionsAttacked( chessPiece, fromPosition, dimension ):
-		scanLine, anyDistance = ChessPiece.getScanLineDistance( chessPiece.pieceName )
+	def positionsAttacked( self, fromPosition, dimension, blockedCells=None ):
+		scanLine, anyDistance = ChessPiece.getScanLineDistance( self.pieceName )
 		rows, cols = dimension
 
 		attackedCells = set()
@@ -30,6 +30,8 @@ class ChessPiece:
 			while True:
 				x, y = u + du * distance, v + dv * distance
 				if not 0 <= x < rows or not 0 <= y < cols:
+					break
+				if blockedCells is not None and (x, y) in blockedCells:
 					break
 				attackedCells.add( (x, y) )
 				distance += 1
@@ -126,7 +128,7 @@ class ChessAvoidance:
 		occupiedCells = set( chessPiecesPlacedDict.keys() )
 		
 		for cell in list( emptyCells ):
-			attackedCells = ChessPiece.positionsAttacked( chessPieceToPlace, cell, self.dimension )
+			attackedCells = chessPieceToPlace.positionsAttacked( cell, self.dimension )
 			if len( set.intersection( attackedCells, occupiedCells ) ) == 0:
 				# We can place the chess piece at this location. Remove attackedCells as well
 				# as the current cell from emptyCells.
@@ -233,6 +235,155 @@ class ChessAvoidanceTest( unittest.TestCase ):
 		
 		for chessPieceInfoString, expectedAvoidanceBoard in zip( chessPieceInfoStringList, expectedAvoidanceBoardList ):
 			self._verify( board, chessPieceInfoString, expectedAvoidanceBoard )
+
+class ChessPath:
+	def __init__( self, board ):
+		self.dimension = self.rows, self.cols = len( board ), len( board[ 0 ] )
+		self.startPosition, self.targetPosition = (self.rows - 1, 0), (0, self.cols - 1)
+
+		self.chessPieceDict = dict()
+		for row, col in itertools.product( range( self.rows ), range( self.cols ) ):
+			token = board[ row ][ col ]
+			if token in ChessPiece.tokenToPieceMap:
+				self.chessPieceDict[ (row, col) ] = ChessPieceFactory.get( ChessPiece.tokenToPieceMap[ token ] )
+
+		self.chessPiecePositionSet = set( self.chessPieceDict.keys() )
+
+		self.board = list()
+		for boardRow in board:
+			self.board.append( list( boardRow ) )
+		
+		self.adjacentCellList = [ (0, 1), (0, -1), (1, 0), (-1, 0) ]
+		self.diagonalAdjacentCellList = [ (1, 1), (1, -1), (-1, 1), (-1, -1) ]
+		self.emptyCellToken, self.pathCellToken = '.', '#'
+
+	def _touch( self, cell, visited ):
+		visitedAdjacentCells = list()
+
+		u, v = cell
+		for du, dv in self.adjacentCellList + self.diagonalAdjacentCellList:
+			cell = u + du, v + dv
+			if cell in visited:
+				visitedAdjacentCells.append( cell )
+		if len( visitedAdjacentCells ) > 2:
+			return True
+		elif len( visitedAdjacentCells ) < 2:
+			return False
+		(r1, c1), (r2, c2) = visitedAdjacentCells
+		return r1 != r2 and c1 != c2
+
+	def _search( self, currentPosition, visited ):
+		cell = row, col = currentPosition
+
+		visited.add( (row, col) )
+		self.board[ row ][ col ] = self.pathCellToken
+
+		if currentPosition == self.targetPosition:
+			attackedCellCount = None
+			for position, chessPiece in self.chessPieceDict.items():
+				attackedCells = chessPiece.positionsAttacked( position, self.dimension, self.chessPiecePositionSet )
+				# How many attackedCells are in visited ?
+				count = len( set.intersection( attackedCells, visited ) )
+				if attackedCellCount is None:
+					attackedCellCount = count
+				elif count != attackedCellCount:
+					# We have reached an invalid state.
+					self.board[ row ][ col ] = self.emptyCellToken
+					visited.remove( cell )
+					return False
+			return True
+
+		# Try to move to an adjacent cell.
+		for du, dv in self.adjacentCellList:
+			newCell = x, y = row + du, col + dv
+			if not 0 <= x < self.rows or not 0 <= y < self.cols:
+				continue
+			if newCell in visited or newCell in self.chessPieceDict:
+				continue
+			if self._touch( newCell, visited ):
+				continue
+			
+			pathFound = self._search( newCell, visited )
+			if pathFound:
+				return True
+
+		self.board[ row ][ col ] = self.emptyCellToken
+		visited.remove( cell )		
+		return False
+
+	def path( self ):
+		visited = set()
+
+		self._search( self.startPosition, visited )
+		
+		# The board is a list of lists. Convert it into a list of strings.
+		for row in range( self.rows ):
+			self.board[ row ] = ''.join( self.board[ row ] )
+
+		return self.board
+
+class BoardReader:
+	@staticmethod
+	def read( inputFile ):
+		rows, cols = map( int, inputFile.readline().strip().split() )
+		board = list()
+		if not rows == cols == 0:
+			for _ in range( rows ):
+				board.append( inputFile.readline().strip() )
+			inputFile.readline()
+		return rows, cols, board
+
+class ChessPathTest( unittest.TestCase ):
+	def _render( self, boardWithPath ):
+		for rowString in boardWithPath:
+			print( rowString )
+		print()
+
+	def test_ChessPath( self ):
+		expectedBoardWithPathList = list()
+		with open ('tests/ChessPath.ans' ) as solutionFile:
+			while True:
+				rows, cols, boardWithPath = BoardReader.read( solutionFile )
+				if rows == cols == 0:
+					break
+				expectedBoardWithPathList.append( boardWithPath )
+
+		index = 0
+		with open( 'tests/ChessPath' ) as inputFile:
+			while True:
+				rows, cols, board = BoardReader.read( inputFile )
+				if rows == cols == 0:
+					break
+
+				expectedBoardWithPath = expectedBoardWithPathList[ index ]
+				index += 1
+				
+				print( 'Testcase #{} rows = {} cols = {}'.format( index, rows, cols ) )
+				boardWithPath = ChessPath( board ).path()
+				
+				self._render( boardWithPath )
+				self.assertEqual( boardWithPath, expectedBoardWithPath )
+
+class RookEndMaze:
+	def __init__( self, board ):
+		pass
+
+	def path( self ):
+		pass
+
+class RookEndMazeTest( unittest.TestCase ):
+	def test_RookEndMaze( self ):
+		index = 0
+		with open( 'tests/RookEndMaze' ) as inputFile:
+			while True:
+				rows, cols, board = BoardReader.read( inputFile )
+				if rows == cols == 0:
+					break
+
+				index += 1
+
+				print( 'Testcase #{} rows = {} cols = {}'.format( index, rows, cols ) )
+				RookEndMaze( board ).path()
 
 if __name__ == '__main__':
 	unittest.main()
